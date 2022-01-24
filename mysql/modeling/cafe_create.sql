0c172bd54887b57d8d64b888f01996c2ace32663
user <user@14.53.248.164>
2022-01-24 13:53:22 +0900

    eee

---
+++ mysql/modeling/cafe_create.sql
@@ -0,0 +1,27 @@
+-- 잊지 말고 꼭 하기
+use world;
+
+
+create table cafe(
+seq int,
+cafename varchar(100),
+tablenum int(100),
+chairnum int(100),
+adress varchar(150),
+area varchar(150),
+ceoname varchar(100)
+);
+
+drop table cafe;
+
+create table cafe(
+seq int,
+cafename varchar(100),
+tablenum int,
+chairnum int,
+adress varchar(150),
+area varchar(150),
+ceoname varchar(100)
+);
+
+drop table cafe;
