1fec3bc917312b6aa127c62ad3563f83a1e5290d
user <user@14.53.248.164>
2022-01-24 13:53:54 +0900

    dddd

--- mysql/modeling/cafe_create.sql
+++ mysql/modeling/cafe_create.sql
@@ -1,3 +1,30 @@
+use world;
+
+show tables;
+
+-- 주석
+
+-- 테이블 생성
+create table member(
+seq int,
+neme varchar(100),
+id varchar(100),
+regdate datetime
+);
+
+create table member2 (
+ seq int,
+ name varchar(100),
+ id varchar(100),
+ regdate datetime
+);
+
+-- 테이블 삭제
+
+drop table member2;
+drop table member;
+drop table member;
+
 -- 잊지 말고 꼭 하기
 use world;
 
